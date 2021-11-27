e5165ddfd8b0f05e46b14880527e565ffd80ad6f
Sanjeet N. Dasharath <sanjeetdasharath@gmail.com>
2021-11-27 11:38:21 +0530

    finalize error fn api

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate nalgebra as na;
 
-use na::{DMatrix, DVector, Matrix};
+use na::{DMatrix, DMatrixSlice, DVector};
+use ndarray::prelude::*;
 use rand::prelude::*;
 use rand_distr::StandardNormal;
 use std::collections::HashMap;
@@ -23,6 +24,28 @@ pub trait Algo {
     fn fit(&mut self);
 }
 
+pub trait ErrorFn {
+    fn call(&mut self, pred: f64, actual: f64) -> f64;
+    fn grad_user(&mut self, pred: f64, actual: f64, p_user: f64, q_item: f64) -> f64;
+    fn grad_item(&mut self, pred: f64, actual: f64, p_user: f64, q_item: f64) -> f64;
+}
+
+pub struct MeanSquaredError {}
+
+impl ErrorFn for MeanSquaredError {
+    fn call(&mut self, pred: f64, actual: f64) -> f64 {
+        (pred - actual) * (pred - actual)
+    }
+
+    fn grad_user(&mut self, pred: f64, actual: f64, _p_user: f64, q_item: f64) -> f64 {
+        2.0 * (pred - actual) * q_item
+    }
+
+    fn grad_item(&mut self, pred: f64, actual: f64, p_user: f64, _q_item: f64) -> f64 {
+        2.0 * (pred - actual) * p_user
+    }
+}
+
 pub struct Svd {
     pub user_bias: Option<DVector<f64>>,
     pub user_factors: Option<DMatrix<f64>>,
@@ -30,10 +53,21 @@ pub struct Svd {
     pub item_factors: Option<DMatrix<f64>>,
     pub num_factors: usize,
     pub num_epochs: usize,
+    pub lr_user: f64,
+    pub lr_item: f64,
+    pub reg_user: f64,
+    pub reg_item: f64,
 }
 
 impl Svd {
-    pub fn new(num_factors: usize, num_epochs: usize) -> Self {
+    pub fn new(
+        num_factors: usize,
+        num_epochs: usize,
+        lr_user: f64,
+        lr_item: f64,
+        reg_user: f64,
+        reg_item: f64,
+    ) -> Self {
         Self {
             user_bias: None,
             user_factors: None,
@@ -41,34 +75,36 @@ impl Svd {
             item_factors: None,
             num_factors,
             num_epochs,
+            lr_user,
+            lr_item,
+            reg_user,
+            reg_item,
         }
     }
 
     pub fn fit(&mut self, dataset: Dataset) {
-        let squared_error_fn = |pred, actual| 2.0 * (pred - actual);
-        self.fit_with(dataset, squared_error_fn)
+        self.fit_with(dataset, MeanSquaredError {})
     }
 
-    pub fn fit_with<F>(&mut self, dataset: Dataset, error_fn: F)
-    where
-        F: Fn(f64, f64) -> f64,
-    {
+    pub fn fit_with(&mut self, dataset: Dataset, mut error_fn: impl ErrorFn) {
         let num_users = dataset.user_to_idx.len();
         let num_items = dataset.item_to_idx.len();
         let num_examples = dataset.values.len();
 
         let randn = |_, _| thread_rng().sample(StandardNormal);
 
-        let user_factors = DMatrix::from_fn(num_users, self.num_factors, randn);
-        let item_factors = DMatrix::from_fn(num_items, self.num_factors, randn);
+        let mut user_factors = DMatrix::from_fn(num_users, self.num_factors, randn);
+        let mut item_factors = DMatrix::from_fn(num_items, self.num_factors, randn);
 
         let user_bias = DVector::zeros(num_users);
         let item_bias = DVector::zeros(num_items);
 
+        // iterate over num epochs
         for epoch in 0..self.num_epochs {
+            // iterate over all training examples
             for i in 0..num_examples {
-                let user_idx = *dataset.user_to_idx.get(&dataset.users[i]).unwrap();
-                let item_idx = *dataset.item_to_idx.get(&dataset.items[i]).unwrap();
+                let user_idx = dataset.user_to_idx[&dataset.users[i]];
+                let item_idx = dataset.item_to_idx[&dataset.items[i]];
 
                 let user_vec = user_factors.row(user_idx);
                 let item_vec = item_factors.row(item_idx);
@@ -77,7 +113,21 @@ impl Svd {
 
                 let pred = bu + bi + user_vec.dot(&item_vec);
                 let actual = dataset.values[i];
-                let error = error_fn(pred, actual);
+
+                let error = error_fn.call(pred, actual);
+
+                for f in 0..self.num_factors {
+                    let p_user: f64 = user_factors[(user_idx, f)];
+                    let q_item: f64 = item_factors[(item_idx, f)];
+
+                    let grad_user = error_fn.grad_user(pred, actual, p_user, q_item);
+                    let grad_item = error_fn.grad_item(pred, actual, p_user, q_item);
+
+                    user_factors[(user_idx, f)] -=
+                        self.lr_user * (grad_user + self.reg_user * p_user);
+                    item_factors[(item_idx, f)] -=
+                        self.lr_item * (grad_item + self.reg_item * q_item);
+                }
             }
         }
 
@@ -85,6 +135,12 @@ impl Svd {
         self.user_factors = Some(user_factors);
         self.item_bias = Some(item_bias);
         self.item_factors = Some(item_factors);
+    }
+}
+
+impl Default for Svd {
+    fn default() -> Self {
+        Svd::new(100, 20, 0.1, 0.1, 0.001, 0.001)
     }
 }
 
@@ -176,7 +232,7 @@ mod tests {
     fn it_works() {
         let csv_reader = CsvReader::new("./test.csv", (0, 1, 2), b'\t', false);
         let dataset = Dataset::new(csv_reader.into_iter());
-        let mut svd = Svd::new(3, 2);
+        let mut svd = Svd::default();
         svd.fit(dataset);
     }
 }
