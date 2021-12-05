c182328e7c6b42ba12840cb55b2d559a4009e50e
Sanjeet Dasharath <sanjeet.dasharath@Sgmail.com>
2021-12-05 10:27:50 +0530

    implement compute_similarities

--- src/algos/knn.rs
+++ src/algos/knn.rs
@@ -1,5 +1,6 @@
+use crate::algos::Algorithm;
 use crate::data::Dataset;
-use crate::similarities::Similarity;
+use crate::similarities::{cosine, msd, Similarity};
 use na::DMatrix;
 
 pub struct Knn {
@@ -21,5 +22,33 @@ impl Knn {
         }
     }
 
-    fn compute_similarities(&mut self, dataset: &Dataset) {}
+    fn compute_similarities(&mut self, dataset: &Dataset) -> DMatrix<f64> {
+        let mut matrix = DMatrix::zeros(dataset.num_users, dataset.num_items);
+
+        for i in 0..dataset.len() {
+            let user = dataset.user_to_idx[&dataset.users[i]];
+            let item = dataset.item_to_idx[&dataset.items[i]];
+            let value = dataset.values[i];
+            matrix[(user, item)] = value;
+        }
+
+        if !self.user_based {
+            matrix.transpose_mut();
+        }
+
+        match self.similarity {
+            Similarity::Cosine => cosine(matrix),
+            Similarity::Msd => msd(matrix),
+        }
+    }
+}
+
+impl Algorithm for Knn {
+    fn fit(&mut self, dataset: &Dataset) {
+        self.sim = Some(self.compute_similarities(dataset));
+    }
+
+    fn predict(&self, user_idx: usize, item_idx: usize) -> f64 {
+        todo!()
+    }
 }
