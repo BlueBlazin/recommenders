fcad1e88a2c42df91a3073490c7cd0cce1094293
Sanjeet Dasharath <sanjeetdasharath@gmail.com>
2021-12-06 12:34:50 +0530

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,3 @@ pub mod data;
 pub mod metrics;
 pub mod objectives;
 pub mod similarities;
-
-/*
-Desired features:
-1. allow training with different loss functions including custom ones
-2. first class support for csv files, easy to use normal csvs (not wrapped) to train
-3. allow transforms and inverse transform data including custom transforms
-4. first class support to query internals such as learned embeddings, mapping
-   values to internal integer indices
-5. simple API for integrating in other programs
-6. a plug and play command line interface
-7. speed
-*/
